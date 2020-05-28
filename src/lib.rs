8ffc8870a772109907f1579744549274c2a78d95
Izzy Swart <zenerboson@gmail.com>
2020-05-28 11:36:51 -0700

    server side

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,35 @@
-use abstract_ws::{Socket as AbstractSocket, SocketProvider, Url};
+use abstract_ws::{ServerProvider, Socket as AbstractSocket, SocketProvider, Url};
 use async_std::{
     io::{Read as AsyncRead, Write as AsyncWrite},
-    net::TcpStream,
+    net::{TcpListener, TcpStream},
 };
 use core::{
     future::Future,
     pin::Pin,
     task::{Context, Poll},
 };
-use futures::{Sink, Stream};
-use std::io::Error;
-use tokio_tungstenite::{client_async, WebSocketStream};
+use futures::{
+    future::{ready, Either, FlattenStream, Map, Ready},
+    ready,
+    stream::{once, Once, Then},
+    FutureExt, Sink, Stream, StreamExt,
+};
+use std::{io::Error, net::SocketAddr};
+use thiserror::Error;
+use tokio_tungstenite::{accept_async, client_async, WebSocketStream};
 use tungstenite::{error::Error as WsError, Message};
 
 pub struct Socket {
-    inner: WebSocketStream<TokioCompat<TcpStream>>,
+    inner: Option<WebSocketStream<TokioCompat<TcpStream>>>,
+    close: Option<Pin<Box<dyn Future<Output = Result<(), WsError>> + Send>>>,
 }
 
+#[derive(Debug, Error)]
 pub enum ConnectError {
-    Io(Error),
-    Ws(WsError),
-    BadUrl,
+    #[error("io error: {0}")]
+    Io(#[source] Error),
+    #[error("ws error: {0}")]
+    Ws(#[source] WsError),
 }
 
 impl From<Error> for ConnectError {
@@ -38,30 +47,43 @@ impl From<WsError> for ConnectError {
 impl Socket {
     fn new(url: Url) -> impl Future<Output = Result<Self, ConnectError>> {
         async move {
-            let stream = TcpStream::connect(url.host_str().ok_or(ConnectError::BadUrl)?).await?;
+            let stream = TcpStream::connect(&*url.socket_addrs(|| match url.scheme() {
+                "ws" => Some(80),
+                "wss" => Some(443),
+                _ => None,
+            })?)
+            .await?;
 
             let (inner, _) = client_async(url, stream.compat()).await?;
 
-            Ok(Socket { inner })
+            Ok(Socket {
+                inner: Some(inner),
+                close: None,
+            })
         }
     }
 }
 
 impl Stream for Socket {
-    type Item = Vec<u8>;
+    type Item = Result<Vec<u8>, WsError>;
 
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Vec<u8>>> {
-        Pin::new(&mut self.inner).poll_next(cx).map(|item| {
-            item.map(|item| {
-                let item = item.ok();
-                item.map(|item| match item {
-                    Message::Binary(data) => Some(data),
-                    _ => None,
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+    ) -> Poll<Option<Result<Vec<u8>, WsError>>> {
+        Pin::new(self.inner.as_mut().expect("polled after close"))
+            .poll_next(cx)
+            .map(|item| {
+                item.map(|item| {
+                    item.map(|item| match item {
+                        Message::Binary(data) => Some(data),
+                        _ => None,
+                    })
                 })
+                .transpose()
+                .map(|item| item.flatten())
+                .transpose()
             })
-            .flatten()
-            .flatten()
-        })
     }
 }
 
@@ -69,19 +91,30 @@ impl Sink<Vec<u8>> for Socket {
     type Error = WsError;
 
     fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
-        Pin::new(&mut self.inner).poll_ready(cx)
+        Pin::new(self.inner.as_mut().expect("polled after close")).poll_ready(cx)
     }
 
     fn start_send(mut self: Pin<&mut Self>, item: Vec<u8>) -> Result<(), Self::Error> {
-        Pin::new(&mut self.inner).start_send(Message::Binary(item))
+        Pin::new(self.inner.as_mut().expect("polled after close")).start_send(Message::Binary(item))
     }
 
     fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
-        Pin::new(&mut self.inner).poll_flush(cx)
+        Pin::new(self.inner.as_mut().expect("polled after close")).poll_flush(cx)
     }
 
     fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
-        Pin::new(&mut self.inner).poll_close(cx)
+        let this = &mut *self;
+
+        loop {
+            if let Some(close) = &mut this.close {
+                return (*close).as_mut().poll(cx);
+            } else {
+                this.close = Some(Box::pin(
+                    once(ready(Ok(Message::Close(None))))
+                        .forward(this.inner.take().expect("invalid state")),
+                ));
+            }
+        }
     }
 }
 
@@ -136,3 +169,89 @@ impl<T: AsyncWrite + Unpin> tokio::io::AsyncWrite for TokioCompat<T> {
 }
 
 impl TokioCompatExt for TcpStream {}
+
+pub struct Server;
+
+impl ServerProvider for Server {
+    type Listen = FlattenStream<
+        Map<
+            Pin<Box<dyn Future<Output = Result<TcpListener, Error>> + Send>>,
+            fn(
+                Result<TcpListener, Error>,
+            ) -> Either<Listen, Once<Ready<Result<Socket, ConnectError>>>>,
+        >,
+    >;
+    type Socket = Socket;
+
+    fn listen(&self, addr: SocketAddr) -> Self::Listen {
+        fn res_conv(
+            item: Result<TcpListener, Error>,
+        ) -> Either<Listen, Once<Ready<Result<Socket, ConnectError>>>> {
+            match item {
+                Ok(listener) => Either::Left(Listen {
+                    listener: Incoming { listener }.then(conv),
+                }),
+                Err(err) => Either::Right(once(ready(Err(err.into())))),
+            }
+        }
+
+        (Box::pin(TcpListener::bind(addr))
+            as Pin<Box<dyn Future<Output = Result<TcpListener, Error>> + Send>>)
+            .map(
+                res_conv
+                    as fn(
+                        Result<TcpListener, Error>,
+                    )
+                        -> Either<Listen, Once<Ready<Result<Socket, ConnectError>>>>,
+            )
+            .flatten_stream()
+    }
+}
+
+pub struct Listen {
+    listener: Then<
+        Incoming,
+        Pin<Box<dyn Future<Output = Result<Socket, ConnectError>> + Send>>,
+        fn(
+            Result<TcpStream, Error>,
+        ) -> Pin<Box<dyn Future<Output = Result<Socket, ConnectError>> + Send>>,
+    >,
+}
+
+pub struct Incoming {
+    listener: TcpListener,
+}
+
+impl Stream for Incoming {
+    type Item = Result<TcpStream, Error>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let future = self.listener.accept();
+        pin_utils::pin_mut!(future);
+
+        let (socket, _) = ready!(future.poll(cx))?;
+        Poll::Ready(Some(Ok(socket)))
+    }
+}
+
+fn conv(
+    stream: Result<TcpStream, Error>,
+) -> Pin<Box<dyn Future<Output = Result<Socket, ConnectError>> + Send>> {
+    Box::pin(async move {
+        let stream = stream?;
+        let inner = accept_async(stream.compat()).await?;
+
+        Ok::<_, ConnectError>(Socket {
+            inner: Some(inner),
+            close: None,
+        })
+    })
+}
+
+impl Stream for Listen {
+    type Item = Result<Socket, ConnectError>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        Pin::new(&mut self.listener).poll_next(cx)
+    }
+}
